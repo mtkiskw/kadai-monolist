9cfe99c0548832da1677de5b7040c03c1e80d04a
Miteki Ishikawa <miteki.i53@gmail.com>
2017-04-21 00:26:52 +0000

    app settings

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,8 +11,7 @@ class ItemsController < ApplicationController
       })
 
       results.each do |result|
-        # 扱い易いように Item としてインスタンスを作成する（保存はしない）
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item
       end
     end
@@ -32,6 +31,11 @@ class ItemsController < ApplicationController
       url: url,
       image_url: image_url,
     }
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
   end
   
 end
--- config/initializers/rakuten_web_service.rb
+++ config/initializers/rakuten_web_service.rb
@@ -1,3 +1,3 @@
 RakutenWebService.configure do |c|
-  c.application_id = ENV['1054762303224226154']
+  c.application_id = ENV['RAKUTEN_APPLICATION_ID']
 end
